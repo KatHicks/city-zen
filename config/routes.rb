c8ec9d9a48b024917af16fb9dc511f5b1a3e2b57
Samir Gossain <samir.gossain@gmail.com>
2017-02-28 17:31:54 +0000

    Added issue/index

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   resources :issues
   resources :tags
 
-  root "issues#index"
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: "issues#index"
 end
