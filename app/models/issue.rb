8a3d4f33a613b27897c590b8217cd4493f6eabb6
Enrico Graziani <mrenrich84@gmail.com>
2017-02-28 15:59:08 +0000

    Displaying images on the issue show page

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -1,5 +1,5 @@
 class Issue < ApplicationRecord
   enum status: [:open, :pending, :resolved]
-  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  has_attached_file :image, :styles => { :big => "600x400", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
--- spec/features/upload_images_features_spec.rb
+++ spec/features/upload_images_features_spec.rb
@@ -6,7 +6,16 @@ feature "User can upload a photo" do
     visit "/issues/new"
     attach_file('issue[image]', Rails.root + 'spec/features/vandal.jpg')
     click_button "Create Issue"
-    save_and_open_page
-    expect(page).to have_xpath("//img[@class='issue_image']")
+    expect(page).to have_xpath("//img[@class='issue_thumb_image']")
   end
+
+  it "shows a big photo in issue details" do
+    visit "/issues/new"
+    attach_file('issue[image]', Rails.root + 'spec/features/vandal.jpg')
+    click_button "Create Issue"
+    visit "/issues/"
+    click_link "Show"
+    expect(page).to have_xpath("//img[@class='issue_big_image']")
+  end
+
 end
