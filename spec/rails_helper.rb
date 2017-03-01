fadf7b2d055aa164162abac79bdaca8c6e85ba6b
Enrico Graziani <mrenrich84@gmail.com>
2017-03-01 14:58:01 +0000

    Fixing db cleaner

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -9,7 +9,7 @@ require 'capybara/rails'
 require 'web_helper'
 require 'capybara/poltergeist'
 Capybara.javascript_driver = :poltergeist
-require 'database_cleaner'
+require 'support/database_cleaner'
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
