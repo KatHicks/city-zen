7f25121419b8a65bd3ca03b5a9c1726b0269895f
Ben Kielty <bwk103@gmail.com>
2017-03-01 14:57:41 +0000

    Adding feature / unit tests for maps

--- spec/models/issues_spec.rb
+++ spec/models/issues_spec.rb
@@ -12,4 +12,10 @@ describe Issue, type: :model do
     expect(issue).not_to be_valid
   end
 
+  it 'should store location data in database' do
+    issue = Issue.create(title: "Ramp broken", description: "This is a test description",
+                      latitude: "51.50", longitude: "-0.118" )
+    expect(Issue.last.latitude).to eq 51.50
+    expect(Issue.last.longitude).to eq -0.118
+  end
 end
--- spec/web_helper.rb
+++ spec/web_helper.rb
@@ -5,3 +5,12 @@ def add_issue(title, description)
   fill_in "Description", with: description
   click_button "Create Issue"
 end
+
+def add_issue_with_location
+  visit new_issue_path
+  fill_in 'Title', with: "A test"
+  fill_in 'Description', with: "A description of the test"
+  first('input#latitude', visible: false).set("51.509865")
+  first('input#longitude', visible: false).set("-0.118092")
+  click_button 'Create Issue'
+end
