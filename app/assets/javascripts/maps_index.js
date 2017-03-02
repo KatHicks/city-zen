18cc5e7c45cb1354f41f5ef493c768951e2700e2
Ben Kielty <bwk103@gmail.com>
2017-03-02 18:14:11 +0000

    Failing work on infowindows

--- app/assets/javascripts/maps_index.js
+++ app/assets/javascripts/maps_index.js
@@ -2,6 +2,7 @@
 function indexMarkers(){
   var markers = gon.latlngs;
   var status = gon.status;
+  var title = "Hello Pedro";
   var index_map = new google.maps.Map(document.getElementById('map'), {
     zoom: 13,
     center: {lat: 51.508672, lng: -0.127233},
@@ -14,10 +15,29 @@ function indexMarkers(){
       marker = new google.maps.Marker({
         position: markers[i],
         map: index_map,
+        title: title
       });
+
+      var infoWindow = new google.maps.InfoWindow();
+      (function (marker, markers.length[i] {
+        google.maps.event.addListener(marker, 'click', function(){
+          var infoContent: "specific information associated"});
+          infoWindow.setContent(infoontent);
+          infowindow.open(map, marker);
+        });
+        (marker, result[i]);
+      }))
       markerStyle(marker, status[i]);
       bounds.extend(marker.position);
     }
+
+    var infowindow = new google.maps.InfoWindow({
+    content: "Hello"
+  });
+
+  marker.addListener('click', function() {
+    infowindow.open(map, marker);
+  });
     index_map.fitBounds(bounds);
   }
 }
