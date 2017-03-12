7b950f68d7e5e8b7e95f5e3a742e2b57d997b58e
Kat Hicks <kmhicks92@gmail.com>
2017-03-12 14:38:42 +0000

    Removed comments from code

--- spec/features/user_roles_feature_spec.rb
+++ spec/features/user_roles_feature_spec.rb
@@ -1,14 +1,15 @@
 require 'rails_helper'
-Cityzen = Struct.new(:email, :title, :description)  # Is this a good way to declare constants to use with Capybara
 
-  feature 'User Access restricted by role' do
+Cityzen = Struct.new(:email, :title, :description)
+
+  feature 'User access restricted by role' do
     context 'As a Cityzen' do
       before do
         @cityzen1 = Cityzen.new('cityzen1@fixcity.com', 'Left my granny', 'She is in the park and she only has a packet of crisps left')
         sign_up(email: @cityzen1.email)
         add_issue(title: @cityzen1.title, description: @cityzen1.description)
         sign_out
-        
+
         @cityzen2 = Cityzen.new('cityzen2@fixcity.com', 'Bad Rabbit', "That's a very bad road blocking rabbit")
         sign_up(email: @cityzen2.email)
         add_issue(title: @cityzen2.title, description: @cityzen2.description)
